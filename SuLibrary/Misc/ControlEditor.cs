9bbe0bbee220e0156b8a6ee1a9a37645cb4ea277
zadamantiy <33583385+zadamantiy@users.noreply.github.com>
2022-12-02 19:22:33 +0300

    Update ControlEditor.cs

--- SuLibrary/Misc/ControlEditor.cs
+++ SuLibrary/Misc/ControlEditor.cs
@@ -5,24 +5,6 @@ namespace SuLibrary.Misc
 {
     public static class ControlEditor
     {
-        /*
-        public void UpdateGraphs(ZedGraphControl control, CheckedListBox graphList)
-        {
-            graphList.Items.Clear();
-            int i = 1;
-            foreach (var curve in control.GraphPane.CurveList)
-            {
-                if (curve.Label.Text != "")
-                {
-                    graphList.Items.Add(curve.Label.Text);
-                }
-                else
-                {
-                    graphList.Items.Add("Unnamed graph " + i++);
-                }
-            }
-        }*/
-
         public static void UpdateGraphs(ZedGraphControl control, ListBox graphList)
         {
             graphList.Items.Clear();
